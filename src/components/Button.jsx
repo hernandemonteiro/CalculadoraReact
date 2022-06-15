72fa746bae923953231f740be5c376323b7fe552
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-06-15 15:15:09 -0300

    Create Button and clean
    Criando Botões e limpando arquivos não uteis

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './Button.css';
 
-export default props => {
-    return (
-        
-    )
-}
+export default props => 
+    <button className='button'>0</button>
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 
+import Button from '../components/Button';
+
 
 export default class Calculator extends Component {
     render() {
         return (
             <div className='Calculator'>
-                
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
             </div>
         )
     }
