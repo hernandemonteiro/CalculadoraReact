7e10020890eb8699be049ebafae9e8187a84d561
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-06-15 18:02:43 -0300

    Implementando a Lógica
    Lógica para calculo com uso do Eval

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,10 +4,17 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialstate = {
+    displayValue: '0',
+    clearDisplay: false,
+    Operation: null,
+    Values: [0, 0],
+    current: 0
+}
 
 export default class Calculator extends Component {
-
-    constructor(props){
+    state = { ...initialstate };
+    constructor(props) {
         super(props);
         this.clearMemory = this.clearMemory.bind(this);
         this.clearEntry = this.clearEntry.bind(this);
@@ -16,7 +23,7 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar');
+        this.setState({ ...initialstate });
     }
 
     clearEntry() {
@@ -24,35 +31,76 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation);
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+
+            const Values = [...this.state.Values];
+            try {
+                Values[0] = eval(`${Values[0]} ${currentOperation} ${Values[1]}`)
+            } catch (e) {
+                Values[0] = this.state.Values[0];
+            }
+
+            Values[1] = 0;
+
+            this.setState({
+                displayValue: Values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                Values
+            })
+        }
     }
 
     addDigit(number) {
-        console.log(number);
+
+        if (number === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay;
+        const CurrentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = CurrentValue + number;
+
+        this.setState({ displayValue, clearDisplay: false });
+
+        if (number !== '.') {
+            const indice = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const Values = [...this.state.Values];
+            Values[indice] = newValue;
+            this.setState({ Values });
+        }
     }
+
     render() {
-        
+
         return (
             <div className='Calculator'>
-                    <Display value={100}/>
-                    <Button label='AC' operation double click={this.clearMemory}/>
-                    <Button label='CE' operation click={this.clearEntry}/>
-                    <Button label='/' operation click={this.setOperation}/>
-                    <Button label='7' click={this.addDigit}/>
-                    <Button label='8' click={this.addDigit}/>
-                    <Button label='9' click={this.addDigit}/>
-                    <Button label='X' operation click={this.setOperation}/>
-                    <Button label='4' click={this.addDigit}/>
-                    <Button label='5' click={this.addDigit}/>
-                    <Button label='6' click={this.addDigit}/>
-                    <Button label='-' operation click={this.setOperation}/>
-                    <Button label='1' click={this.addDigit}/>
-                    <Button label='2' click={this.addDigit}/>
-                    <Button label='3' click={this.addDigit}/>
-                    <Button label='+' operation click={this.setOperation}/>
-                    <Button label='0' double click={this.addDigit}/>
-                    <Button label='.' click={this.addDigit}/>
-                    <Button label='=' operation click={this.setOperation}/>
+                <Display value={this.state.displayValue} />
+                <Button label='AC' operation double click={this.clearMemory} />
+                <Button label='CE' operation click={this.clearEntry} />
+                <Button label='/' operation click={this.setOperation} />
+                <Button label='7' click={this.addDigit} />
+                <Button label='8' click={this.addDigit} />
+                <Button label='9' click={this.addDigit} />
+                <Button label='*' operation click={this.setOperation} />
+                <Button label='4' click={this.addDigit} />
+                <Button label='5' click={this.addDigit} />
+                <Button label='6' click={this.addDigit} />
+                <Button label='-' operation click={this.setOperation} />
+                <Button label='1' click={this.addDigit} />
+                <Button label='2' click={this.addDigit} />
+                <Button label='3' click={this.addDigit} />
+                <Button label='+' operation click={this.setOperation} />
+                <Button label='0' double click={this.addDigit} />
+                <Button label='.' click={this.addDigit} />
+                <Button label='=' operation click={this.setOperation} />
 
             </div>
         )
